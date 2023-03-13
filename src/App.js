65e505d1f206529d38e0a0a2a64b98d7e5d73ca5
khoon <khoon7624@gmail.com>
2023-03-13 17:52:28 +0900

    update

--- src/App.js
+++ src/App.js
@@ -6,27 +6,33 @@ const App = () => {
   const expenses = [
     {
       id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
+      title: "화장지",
+      amount: 120000,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "TV", amount: 1040000, date: new Date(2021, 2, 12) },
     {
       id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
+      title: "자동차 보험",
+      amount: 1100000,
       date: new Date(2021, 2, 28),
     },
     {
       id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
+      title: "책상",
+      amount: 587000,
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log("App.js에서 실행됨");
+    console.log(expense);
+  };
+
   return (
     <div className="App">
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
